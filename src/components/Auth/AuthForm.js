f213f30c944bbaace35fb7882c6d469ef41bb172
Huy Nguyen <huykst137@gmail.com>
2022-08-20 13:46:29 +0700

    Adding user login

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -3,84 +3,93 @@ import {useRef, useState} from 'react';
 import classes from './AuthForm.module.css';
 
 const AuthForm = () => {
-  const emailInputRef = useRef();
-  const passwordInputRef = useRef()
+    const emailInputRef = useRef();
+    const passwordInputRef = useRef()
 
-  const [isLogin, setIsLogin] = useState(true);
-  const [isLoading, setIsLoading] = useState(false)
+    const [isLogin, setIsLogin] = useState(true);
+    const [isLoading, setIsLoading] = useState(false)
 
 
-  const switchAuthModeHandler = () => {
-    setIsLogin((prevState) => !prevState);
-  };
+    const switchAuthModeHandler = () => {
+        setIsLogin((prevState) => !prevState);
+    };
 
-  const submitHandler = (event) => {
-    event.preventDefault();
+    const submitHandler = (event) => {
+        event.preventDefault();
 
-    const enteredEmail = emailInputRef.current.value;
-    const enteredPassword = passwordInputRef.current.value;
+        const enteredEmail = emailInputRef.current.value;
+        const enteredPassword = passwordInputRef.current.value;
 
-  //  optional: Add validation
-      setIsLoading(true);
-    if(isLogin) {
-
-    } else {
-      fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCo231H8zZjgrng9B1LJA0OCleFR4ndrZY',
-          {
-            method: "POST",
-            body: JSON.stringify({
-              email: enteredEmail,
-              password: enteredPassword,
-              returnSecureToken: true
-            }),
-            headers: {
-              'Content-Type': 'application/json'
-            }
-          })
-          .then(res => {
-              setIsLoading(false)
-              if(res.ok) {
-              //    ....
-              } else {
-                  console.log(res)
-                  return res.json().then(data => {
-                      let errorMessage = 'Authentication failed!';
-                      if(data?.error?.message) {
-                          errorMessage =  data.error.message;
-                      }
-                      alert(errorMessage)
-                  });
-              }
-      })
+        //  optional: Add validation
+        setIsLoading(true);
+        let url;
+        if (isLogin) {
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCo231H8zZjgrng9B1LJA0OCleFR4ndrZY'
+        } else {
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCo231H8zZjgrng9B1LJA0OCleFR4ndrZY'
+        }
+        fetch(url,
+            {
+                method: "POST",
+                body: JSON.stringify({
+                    email: enteredEmail,
+                    password: enteredPassword,
+                    returnSecureToken: true
+                }),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(res => {
+                setIsLoading(false)
+                if (res.ok) {
+                    return res.json();
+                } else {
+                    console.log(res)
+                    return res.json().then(data => {
+                        console.log(data)
+                        let errorMessage = 'Authentication failed!';
+                        if (data?.error?.message) {
+                            errorMessage = data.error.message;
+                        }
+                        throw new Error(errorMessage)
+                    });
+                }
+            })
+            .then(data => {
+                console.log(data)
+            })
+            .catch((err) => {
+                alert(err.message)
+            })
     }
-  }
 
-  return (
-    <section className={classes.auth}>
-      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form onSubmit={submitHandler}>
-        <div className={classes.control}>
-          <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required ref={emailInputRef}/>
-        </div>
-        <div className={classes.control}>
-          <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required ref={passwordInputRef}/>
-        </div>
-        <div className={classes.actions}>
-            {!isLoading && <button>{isLogin ? 'Login' : 'Create Account'}</button>}
-            {isLoading && <p>Sending request...</p>}
-          <button
-            type='button'
-            className={classes.toggle}
-            onClick={switchAuthModeHandler}
-          >
-            {isLogin ? 'Create new account' : 'Login with existing account'}
-          </button>
-        </div>
-      </form>
-    </section>
-  );
+    return (
+        <section className={classes.auth}>
+            <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
+            <form onSubmit={submitHandler}>
+                <div className={classes.control}>
+                    <label htmlFor='email'>Your Email</label>
+                    <input type='email' id='email' required ref={emailInputRef}/>
+                </div>
+                <div className={classes.control}>
+                    <label htmlFor='password'>Your Password</label>
+                    <input type='password' id='password' required ref={passwordInputRef}/>
+                </div>
+                <div className={classes.actions}>
+                    {!isLoading && <button>{isLogin ? 'Login' : 'Create Account'}</button>}
+                    {isLoading && <p>Sending request...</p>}
+                    <button
+                        type='button'
+                        className={classes.toggle}
+                        onClick={switchAuthModeHandler}
+                    >
+                        {isLogin ? 'Create new account' : 'Login with existing account'}
+                    </button>
+                </div>
+            </form>
+        </section>
+    );
 };
 
 export default AuthForm;
